604395e57190cc7447eb4322a8f9bcdc33332b52
suecarmol <scardenasmolinar@wikimedia.org>
2020-11-10 13:26:09 -0600

    Add ProjectAggregate admin

--- extlinks/aggregates/admin.py
+++ extlinks/aggregates/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import LinkAggregate, PageAggregate
+from .models import LinkAggregate, PageAggregate, ProjectAggregate
 
 
 class LinkAggregateAdmin(admin.ModelAdmin):
@@ -30,3 +30,18 @@ class PageAggregateAdmin(admin.ModelAdmin):
 
 
 admin.site.register(PageAggregate, PageAggregateAdmin)
+
+
+class ProjectAggregateAdmin(admin.ModelAdmin):
+    list_display = (
+        "organisation",
+        "collection",
+        "project_name",
+        "full_date",
+        "total_links_added",
+        "total_links_removed",
+    )
+    list_filter = ("organisation", "collection", "month", "year")
+
+
+admin.site.register(ProjectAggregate, ProjectAggregateAdmin)
