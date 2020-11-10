612d25a7fdaf480bf2a74b3554b60d3e734d3495
suecarmol <scardenasmolinar@wikimedia.org>
2020-11-09 23:54:04 -0600

    Add PageAggregate admin page

--- extlinks/aggregates/admin.py
+++ extlinks/aggregates/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import LinkAggregate
+from .models import LinkAggregate, PageAggregate
 
 
 class LinkAggregateAdmin(admin.ModelAdmin):
@@ -15,3 +15,18 @@ class LinkAggregateAdmin(admin.ModelAdmin):
 
 
 admin.site.register(LinkAggregate, LinkAggregateAdmin)
+
+
+class PageAggregateAdmin(admin.ModelAdmin):
+    list_display = (
+        "organisation",
+        "collection",
+        "page_name",
+        "full_date",
+        "total_links_added",
+        "total_links_removed",
+    )
+    list_filter = ("organisation", "collection", "month", "year")
+
+
+admin.site.register(PageAggregate, PageAggregateAdmin)
