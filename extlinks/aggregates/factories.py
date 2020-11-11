5553429ef44c251bffae5598ed9a5efcfb0325bd
suecarmol <scardenasmolinar@wikimedia.org>
2020-11-10 20:03:52 -0600

    Add tests for ProjectAggregate management command

--- extlinks/aggregates/factories.py
+++ extlinks/aggregates/factories.py
@@ -2,7 +2,7 @@ import factory
 import random
 import datetime
 
-from .models import LinkAggregate, PageAggregate
+from .models import LinkAggregate, PageAggregate, ProjectAggregate
 from extlinks.organisations.factories import CollectionFactory, OrganisationFactory
 
 
@@ -30,6 +30,23 @@ class PageAggregateFactory(factory.django.DjangoModelFactory):
     organisation = factory.SubFactory(OrganisationFactory)
     collection = factory.SubFactory(CollectionFactory)
     page_name = factory.Faker("word")
+    full_date = factory.Faker(
+        "date_between_dates",
+        date_start=datetime.date(2017, 1, 1),
+        date_end=datetime.date(2020, 10, 31),
+    )
+    total_links_added = random.randint(0, 100)
+    total_links_removed = random.randint(0, 80)
+
+
+class ProjectAggregateFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ProjectAggregate
+        strategy = factory.CREATE_STRATEGY
+
+    organisation = factory.SubFactory(OrganisationFactory)
+    collection = factory.SubFactory(CollectionFactory)
+    project_name = factory.Faker("word")
     full_date = factory.Faker(
         "date_between_dates",
         date_start=datetime.date(2017, 1, 1),
--- extlinks/aggregates/tests.py
+++ extlinks/aggregates/tests.py
@@ -4,12 +4,13 @@ from datetime import datetime, date, timedelta
 from django.core.management import call_command
 from django.test import TestCase
 
-from .factories import LinkAggregateFactory, PageAggregateFactory
-from .models import LinkAggregate, PageAggregate
-from extlinks.links.factories import (
-    LinkEventFactory,
-    URLPatternFactory,
+from .factories import (
+    LinkAggregateFactory,
+    PageAggregateFactory,
+    ProjectAggregateFactory,
 )
+from .models import LinkAggregate, PageAggregate, ProjectAggregate
+from extlinks.links.factories import LinkEventFactory, URLPatternFactory
 from extlinks.organisations.factories import CollectionFactory, OrganisationFactory
 
 
@@ -262,6 +263,152 @@ class PageAggregateCommandTest(TestCase):
             organisation=self.organisation,
             collection=self.collection,
             page_name="Page1",
+            full_date=yesterday.date(),
+        )
+
+        self.assertEqual(updated_page_aggregate.total_links_added, 2)
+        self.assertEqual(updated_page_aggregate.total_links_removed, 0)
+
+
+class ProjectAggregateCommandTest(TestCase):
+    def setUp(self):
+        # Creating one Collection
+        self.organisation = OrganisationFactory(name="ACME Org")
+        self.collection = CollectionFactory(name="ACME", organisation=self.organisation)
+        self.url = URLPatternFactory(url="www.google.com", collection=self.collection)
+
+        # Adding LinkEvents so that the command has information to parse.
+        link_event_1 = LinkEventFactory(
+            domain="en.wiki.org", timestamp=datetime(2020, 1, 1, 15, 30, 35)
+        )
+        link_event_1.url.add(self.url)
+        link_event_1.save()
+
+        link_event_2 = LinkEventFactory(
+            domain="es.wiki.org", timestamp=datetime(2020, 1, 1, 17, 40, 55)
+        )
+        link_event_2.url.add(self.url)
+        link_event_2.save()
+
+        link_event_3 = LinkEventFactory(
+            domain="es.wiki.org", timestamp=datetime(2020, 1, 1, 19, 5, 42)
+        )
+        link_event_3.url.add(self.url)
+        link_event_3.save()
+
+        link_event_4 = LinkEventFactory(
+            domain="en.wiki.org", timestamp=datetime(2020, 9, 10, 12, 9, 14)
+        )
+        link_event_4.url.add(self.url)
+        link_event_4.save()
+
+        link_event_5 = LinkEventFactory(
+            domain="en.wiki.org", timestamp=datetime(2020, 9, 10, 12, 40, 50)
+        )
+        link_event_5.url.add(self.url)
+        link_event_5.save()
+
+        link_event_6 = LinkEventFactory(
+            domain="en.wiki.org", timestamp=datetime(2020, 9, 10, 16, 52, 49)
+        )
+        link_event_6.url.add(self.url)
+        link_event_6.save()
+
+        link_event_7 = LinkEventFactory(
+            domain="en.wiki.org", timestamp=datetime(2020, 9, 10, 17, 16, 30)
+        )
+        link_event_7.url.add(self.url)
+        link_event_7.save()
+
+        link_event_8 = LinkEventFactory(
+            domain="es.wiki.org", timestamp=datetime(2020, 9, 10, 22, 36, 15)
+        )
+        link_event_8.url.add(self.url)
+        link_event_8.save()
+
+    # Test when ProjectAggregate table is empty
+    def test_page_aggregate_table_empty(self):
+        self.assertEqual(ProjectAggregate.objects.count(), 0)
+
+        call_command("fill_project_aggregates")
+
+        self.assertEqual(ProjectAggregate.objects.count(), 4)
+
+        # Getting ProjectAggregates to check information is correct
+        enwiki_aggregate_january = ProjectAggregate.objects.get(
+            day=1, month=1, year=2020, project_name="en.wiki.org"
+        )
+        eswiki_aggregate_january = ProjectAggregate.objects.get(
+            day=1, month=1, year=2020, project_name="es.wiki.org"
+        )
+        self.assertEqual(enwiki_aggregate_january.total_links_added, 1)
+        self.assertEqual(eswiki_aggregate_january.total_links_added, 2)
+
+        enwiki_aggregate_september = ProjectAggregate.objects.get(
+            day=10, month=9, year=2020, project_name="en.wiki.org"
+        )
+        eswiki_aggregate_september = ProjectAggregate.objects.get(
+            day=10, month=9, year=2020, project_name="es.wiki.org"
+        )
+        self.assertEqual(enwiki_aggregate_september.total_links_added, 4)
+        self.assertEqual(eswiki_aggregate_september.total_links_added, 1)
+
+    def test_page_aggregate_table_with_data(self):
+        yesterday = datetime.today() - timedelta(days=1)
+        self.assertEqual(ProjectAggregate.objects.count(), 0)
+        # Add ProjectAggregates NOTE: the last ProjectAggregate date here needs to be
+        # greater than the latest LinkEvent date in the setUp
+        ProjectAggregateFactory(full_date=date(2020, 1, 1))
+        ProjectAggregateFactory(full_date=date(2020, 2, 12))
+        ProjectAggregateFactory(full_date=date(2020, 2, 15))
+        ProjectAggregateFactory(full_date=date(2020, 9, 10))
+        ProjectAggregateFactory(full_date=date(2020, 9, 30))
+        ProjectAggregateFactory(full_date=date(2020, 9, 30))
+
+        self.assertEqual(ProjectAggregate.objects.count(), 6)
+
+        # Add a LinkEvent from yesterday, a ProjectAggregate with this date does not
+        # exist yet
+        yesterday_datetime = datetime(
+            yesterday.year, yesterday.month, yesterday.day, 9, 18, 47
+        )
+        link_event = LinkEventFactory(
+            timestamp=yesterday_datetime, domain="en.wiki.org"
+        )
+        link_event.url.add(self.url)
+        link_event.save()
+
+        call_command("fill_project_aggregates")
+
+        self.assertEqual(ProjectAggregate.objects.count(), 7)
+
+        yesterday_page_aggregate = ProjectAggregate.objects.get(
+            full_date=yesterday.date(),
+            project_name="en.wiki.org",
+            collection=self.collection,
+            organisation=self.organisation,
+        )
+        self.assertEqual(yesterday_page_aggregate.total_links_added, 1)
+
+        # The eventstream container crashed and a LinkEvent was not added to
+        # yesterday's aggregate
+        yesterday_late_datetime = datetime(
+            yesterday.year, yesterday.month, yesterday.day, 12, 30, 56
+        )
+        link_event_2 = LinkEventFactory(
+            timestamp=yesterday_late_datetime, domain="en.wiki.org"
+        )
+        link_event_2.url.add(self.url)
+        link_event_2.save()
+
+        # That should be picked up by the command and add the new count
+        call_command("fill_project_aggregates")
+
+        # Now, the link aggregate should have 2 event links added in
+        updated_page_aggregate = ProjectAggregate.objects.get(
+            organisation=self.organisation,
+            collection=self.collection,
+            project_name="en.wiki.org",
             full_date=yesterday.date(),
         )
 
