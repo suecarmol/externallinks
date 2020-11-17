c26e3151d1b3e306e4f96b019f7385484869c6c6
suecarmol <scardenasmolinar@wikimedia.org>
2020-11-16 18:24:47 -0600

    Add tests for UserAggregate management command

--- extlinks/aggregates/factories.py
+++ extlinks/aggregates/factories.py
@@ -2,7 +2,7 @@ import factory
 import random
 import datetime
 
-from .models import LinkAggregate, PageAggregate, ProjectAggregate
+from .models import LinkAggregate, PageAggregate, ProjectAggregate, UserAggregate
 from extlinks.organisations.factories import CollectionFactory, OrganisationFactory
 
 
@@ -47,6 +47,23 @@ class ProjectAggregateFactory(factory.django.DjangoModelFactory):
     organisation = factory.SubFactory(OrganisationFactory)
     collection = factory.SubFactory(CollectionFactory)
     project_name = factory.Faker("word")
+    full_date = factory.Faker(
+        "date_between_dates",
+        date_start=datetime.date(2017, 1, 1),
+        date_end=datetime.date(2020, 10, 31),
+    )
+    total_links_added = random.randint(0, 100)
+    total_links_removed = random.randint(0, 80)
+
+
+class UserAggregateFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = UserAggregate
+        strategy = factory.CREATE_STRATEGY
+
+    organisation = factory.SubFactory(OrganisationFactory)
+    collection = factory.SubFactory(CollectionFactory)
+    username = factory.Faker("name")
     full_date = factory.Faker(
         "date_between_dates",
         date_start=datetime.date(2017, 1, 1),
